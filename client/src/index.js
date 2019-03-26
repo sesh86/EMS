32a9e9f0659308b83e2e4249779d8d366c9ce176
sesh86 <s.seshachalam@gmail.com>
2019-03-26 10:01:01 +0530

    React Project, redux and  route setup

---
+++ client/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Login from './Components/Login'
+
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './Reducers/rootReducer'
+import * as serviceWorker from './serviceWorker';
+import {BrowserRouter, Route} from 'react-router-dom'
+
+const store = createStore(rootReducer);
+
+
+
+ReactDOM.render(
+<Provider store={store}>
+<BrowserRouter>
+<Route exact path="/Login" component={Login}/>
+</BrowserRouter>
+</Provider>
+, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
